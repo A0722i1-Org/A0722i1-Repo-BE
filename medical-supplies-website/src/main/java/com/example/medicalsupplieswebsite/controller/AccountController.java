96e35b7d4a9c938a4a2c31eb5cf8adfce21feaf3
thanhnguyen568 <thanhnguyen230@gmail.com>
2023-07-14 19:55:13 +0700

    login add

--- medical-supplies-website/src/main/java/com/example/medicalsupplieswebsite/controller/AccountController.java
+++ medical-supplies-website/src/main/java/com/example/medicalsupplieswebsite/controller/AccountController.java
@@ -2,6 +2,7 @@ package com.example.medicalsupplieswebsite.controller;
 
 import com.example.medicalsupplieswebsite.service.impl.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "http://localhost:4200")
