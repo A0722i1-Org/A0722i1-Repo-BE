e9c08349e28526e8a27102facd106c8a2daaf183
nlkhanh <nglekhanh810@gmail.com>
2023-07-17 08:06:23 +0700

    Revert "up"
    This reverts commit 26f3cafd00a65548df05706247c8cf8c038ff6dd, reversing
    changes made to 1956887fb257eaaeb8c80842840735b498c6f4e2.

--- medical-supplies-website/src/main/java/com/example/medicalsupplieswebsite/repository/IPositionRepository.java
+++ medical-supplies-website/src/main/java/com/example/medicalsupplieswebsite/repository/IPositionRepository.java
@@ -12,7 +12,6 @@ public interface IPositionRepository extends JpaRepository<Position, Long> {
     /**
      * Created by: PhongTD
      * Date created: 12/07/2023
-     *
      * @return List position
      */
     @Query("SELECT position FROM Position position")
@@ -21,7 +20,6 @@ public interface IPositionRepository extends JpaRepository<Position, Long> {
     /**
      * Created by: PhongTD
      * Date created: 12/07/2023
-     *
      * @param id
      * @return position was found by id
      */
--- medical-supplies-website/src/main/java/com/example/medicalsupplieswebsite/service/IPositionService.java
+++ medical-supplies-website/src/main/java/com/example/medicalsupplieswebsite/service/IPositionService.java
@@ -7,5 +7,4 @@ import java.util.List;
 public interface IPositionService {
     List<Position> findAll();
     Position findById(Long id);
-
 }
