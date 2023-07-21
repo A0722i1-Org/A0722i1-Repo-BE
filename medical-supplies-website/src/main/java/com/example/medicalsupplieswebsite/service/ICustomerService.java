d5c96bcfea90914dd43a47795d0b95ef8df1f701
lduchieu512 <lduchieu512@gmail.com>
2023-07-21 17:53:09 +0700

    HieuLD create & edit

--- medical-supplies-website/src/main/java/com/example/medicalsupplieswebsite/service/ICustomerService.java
+++ medical-supplies-website/src/main/java/com/example/medicalsupplieswebsite/service/ICustomerService.java
@@ -1,6 +1,12 @@
 package com.example.medicalsupplieswebsite.service;
 
+import com.example.medicalsupplieswebsite.dto.CustomerInfo;
 import com.example.medicalsupplieswebsite.entity.Customer;
 
 public interface ICustomerService extends IService<Customer>{
+    void saveCustomer(CustomerInfo customerInfo);
+
+
+    void update(CustomerInfo customerInfo, Long id);
+
 }
--- medical-supplies-website/src/main/java/com/example/medicalsupplieswebsite/service/impl/CustomerService.java
+++ medical-supplies-website/src/main/java/com/example/medicalsupplieswebsite/service/impl/CustomerService.java
@@ -1,15 +1,15 @@
 package com.example.medicalsupplieswebsite.service.impl;
 
-import com.example.medicalsupplieswebsite.entity.Account;
+import com.example.medicalsupplieswebsite.dto.CustomerInfo;
 import com.example.medicalsupplieswebsite.entity.Customer;
-import com.example.medicalsupplieswebsite.repository.IAccountRepository;
 import com.example.medicalsupplieswebsite.repository.ICustomerRepository;
 import com.example.medicalsupplieswebsite.service.ICustomerService;
-import com.example.medicalsupplieswebsite.service.IService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+
 
 @Service
 public class CustomerService implements ICustomerService {
@@ -23,16 +23,36 @@ public class CustomerService implements ICustomerService {
 
     @Override
     public Page<Customer> findAll(Pageable pageable) {
-        return null;
+        return customerRepository.findAll(pageable);
+    }
+
+    @Override
+    public void saveCustomer(CustomerInfo customerInfo) {
+        customerRepository.insertCustomer(customerInfo.getName(),customerInfo.getEmail(),customerInfo.getPhone(),
+                customerInfo.isGender(),customerInfo.getDateOfBirth(),customerInfo.getIdCard(),
+                customerInfo.getCustomerAddress(), customerInfo.getCustomerImg(), customerInfo.getCustomerType(),
+                customerInfo.getCustomerCode(),false);
+
     }
 
     @Override
     public Customer findById(Long id) {
-        return null;
+        return customerRepository.findById(id).orElse(null);
+    }
+
+
+    @Override
+    public void update(CustomerInfo customerInfo, Long id) {
+        customerRepository.updateCustomer(id, customerInfo.getName(),customerInfo.getEmail(),customerInfo.getPhone(),
+                customerInfo.isGender(),customerInfo.getDateOfBirth(),customerInfo.getIdCard(),
+                customerInfo.getCustomerAddress(), customerInfo.getCustomerImg(), customerInfo.getCustomerType(),
+                customerInfo.getCart(),customerInfo.getAccount(),customerInfo.getCustomerCode(),false);
+
     }
 
     @Override
     public Customer save(Customer customer) {
+
         return null;
     }
 
