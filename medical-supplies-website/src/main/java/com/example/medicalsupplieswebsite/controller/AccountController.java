ba4de4f13c1077264dc0d91b0014b192e9e6fe24
nlkhanh <nglekhanh810@gmail.com>
2023-07-25 11:11:50 +0700

    Fix constructor AccountController

--- medical-supplies-website/src/main/java/com/example/medicalsupplieswebsite/controller/AccountController.java
+++ medical-supplies-website/src/main/java/com/example/medicalsupplieswebsite/controller/AccountController.java
@@ -1,7 +1,6 @@
 package com.example.medicalsupplieswebsite.controller;
 
 import com.example.medicalsupplieswebsite.entity.Account;
-import com.example.medicalsupplieswebsite.entity.Employee;
 import com.example.medicalsupplieswebsite.dto.ChangePasswordDto;
 import com.example.medicalsupplieswebsite.service.impl.AccountService;
 import com.example.medicalsupplieswebsite.service.impl.EmployeeService;
@@ -18,20 +17,12 @@ import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.*;
 
-
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -39,13 +30,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountController {
     private final AccountService accountService;
     private final EmployeeService employeeService;
+
     @Autowired
     private AuthenticationManager authenticationManager;
 
-    public AccountController(AccountService accountService, EmployeeService employeeService) {
+    @Autowired
+    AccountController(AccountService accountService, EmployeeService employeeService) {
         this.accountService = accountService;
         this.employeeService = employeeService;
     }
+
 
     /*ThienTDV thêm Tài khoản và setRole cho tài khoản*/
     @PostMapping("/addAccount")
@@ -69,14 +63,10 @@ public class AccountController {
 
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
-        /**
-         * A0722I1-NhanTQ
-         */
 
-    @Autowired
-    AccountController(AccountService accountService) {
-        this.accountService = accountService;
-    }
+    /**
+     * A0722I1-NhanTQ
+     */
 
     @PatchMapping("change-password")
     public ResponseEntity<?> changePassword(@RequestBody ChangePasswordDto changePasswordDto) {
