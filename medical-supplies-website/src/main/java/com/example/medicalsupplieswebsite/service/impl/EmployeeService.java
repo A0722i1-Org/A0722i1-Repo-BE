acaaa7d852394eb204d752dd19ea77e56ccbb42d
vannguyen2006 <vannguyen2006@gmail.com>
2023-07-26 21:06:38 +0700

    update homeController

--- medical-supplies-website/src/main/java/com/example/medicalsupplieswebsite/service/impl/EmployeeService.java
+++ medical-supplies-website/src/main/java/com/example/medicalsupplieswebsite/service/impl/EmployeeService.java
@@ -61,7 +61,6 @@ public class EmployeeService implements IEmployeeService {
      */
     @Override
     public Employee findById(Long id) {
-        return iEmployeeRepository.findAllById(id);
         return null;
     }
 
