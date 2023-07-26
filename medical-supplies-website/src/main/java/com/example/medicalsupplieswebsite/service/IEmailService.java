d5479fe2db91bc25b7e61d28b28f70d95bc27e0e
nhatlh91 <nhatlg@gmail.com>
2023-07-26 16:30:10 +0700

    Them chuc nang VnPay

--- medical-supplies-website/src/main/java/com/example/medicalsupplieswebsite/service/IEmailService.java
+++ medical-supplies-website/src/main/java/com/example/medicalsupplieswebsite/service/IEmailService.java
@@ -1,9 +1,11 @@
 package com.example.medicalsupplieswebsite.service;
 
 import com.example.medicalsupplieswebsite.dto.EmailDetails;
+import com.example.medicalsupplieswebsite.entity.Cart;
 
 public interface IEmailService {
     //Author: NhatLH
     String sendSimpleMail(EmailDetails emailDetails);
 
+    void emailProcess(Cart cart, int totalAmount);
 }
